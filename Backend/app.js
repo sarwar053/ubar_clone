e314cab01008728f1df2741785cf7530f9c30857
sarwar053 <sarwarhossain5353@gmail.com>
2025-10-01 10:07:19 +0600

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv').config();
+const cors = require('cors')
+const express =require('express');
+
+
+const app = express();
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+module.exports = app
